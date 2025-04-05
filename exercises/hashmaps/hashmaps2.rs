4ad82d427995acbdff5e122ddcc23c49b906967d
Meng Cheng <mengc288@gmail.com>
2025-04-05 17:49:06 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -36,10 +36,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
+
+    // 遍历所有水果类型
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        // 如果水果尚未存在于篮子中
+        if !basket.contains_key(&fruit) {
+            // 插入新水果，并设置初始数量为 1
+            basket.insert(fruit, 1);
+
+        }
     }
 }
 
